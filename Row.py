b5baf686a4b3027725cd3ff5cbe103f034d3feea
AlexChilin <super.chilin@mail.ru>
2024-07-01 16:53:38 +0300

    Update and rename Row.py черновик to Row.py

---
+++ Row.py
@@ -0,0 +1,21 @@
+class Row:
+    def __init__(self):
+        self.cards = []
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def take_cards(self):
+        cards = self.cards.copy()
+        self.cards.clear()
+        return cards
+
+    def save(self):
+        return json.dumps({'cards': [card.save() for card in self.cards]})
+
+    @classmethod
+    def load(cls, data):
+        data = json.loads(data)
+        row = cls()
+        row.cards = [Card.load(card_data) for card_data in data['cards']]
+        return row
