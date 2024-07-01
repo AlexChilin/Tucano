0668289bca256b8ee0fb91df1e09bf82adb3f160
AlexChilin <super.chilin@mail.ru>
2024-07-01 16:46:43 +0300

    Update Hand.py

--- Hand.py
+++ Hand.py
@@ -4,14 +4,34 @@ class Hand:
     def __init__(self):
         self.cards = []
 
-    def __repr__(self):
-        cards = []
-        for i in self.cards:
-            cards.append(value)
-        return str(cards)
+    def __str__(self):
+        return ' '.join(str(card) for card in self.cards)
 
-    def add_card(self, cards):
+    #def __repr__(self):
+        #cards = []
+        #for i in self.cards:
+            #cards.append(i.value)
+        #return str(cards)
+
+    def add_cards(self, cards):
         for card in cards:
-            self.cards.append(Card)
+            self.cards.append(cards)
+
+    def score(self):
+        pointes = 0
+        for card in self.cards:
+            pointes += i.point
+        return pointes
+
+    def save(self):
+        return json.dumps({'cards': [card.save() for card in self.cards]})
+
+    @classmethod
+    def load(cls, data):
+        data = json.loads(data)
+        cards = [Card.load(card_data) for card_data in data['cards']]
+        hand = cls()
+        hand.add_cards(cards)
+        return hand
 
     
