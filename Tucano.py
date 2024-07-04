3091e20b9c6c1f1158631d14e9ee1f21860c6add
AlexChilin <super.chilin@mail.ru>
2024-07-04 16:32:41 +0300

    Update Tucano.py

--- Tucano.py
+++ Tucano.py
@@ -61,7 +61,7 @@ class Hand:
                 if hands is not None:
                     counts = [list(map(str, hand.cards)).count(k) for hand in hands]
                     leader = (max(counts) == v)
-            s += Card.score(k, v, leader)
+            s += Card.score(Card(k), v, leader)
         return s
 
     def get_dictionary(self):
@@ -227,77 +227,59 @@ class GameState:
 
 
 class GameInteractions:
-    MAX_PLAYERS = 4
-    COUNT_ROWS = 3
-    SAVE_FILE = 'save-file.json'
+    def __init__(self):
+        self.game_state = None
 
-    @classmethod
-    def run(cls):
-        exist_last_game = cls.is_exist_last_game()
+    def run(self):
+        self.game_state = self.load()
+        self.request_players()
+        self.game_state.add_cards()
 
-        gs = GameState([], [])
-        if exist_last_game:
-            last_gs = GameState.load(cls.SAVE_FILE)
-            print(f'Previous game found: {last_gs}')
-            ans = input('Enter "yes" to start a new game: ')
-            if ans.lower() == 'yes':
-                gs = cls.start_new_game()
-            else:
-                gs = last_gs
-                print('Game loaded')
-        else:
-            gs = cls.start_new_game()
+        while len(self.game_state.rows) > 1:
+            self.print_rows()
+            self.print_player()
 
-        total_moves = gs.deck.remaining_cards() // (len(gs.players) * cls.COUNT_ROWS)
-        for move in range(total_moves):
-            print('-------------------------')
-            print(f'Move {move + 1}')
-            for i in range(len(gs.players)):
-                print('Rows:')
-                cls.print_rows(gs.rows)
-                current_mover = gs.players[gs.current_player]
-                current_mover.choose_row(gs.rows, gs.players)
-
-                gs.add_cards()
-                gs.current_player = (gs.current_player + 1) % len(gs.players)
-                gs.save()
-
-        hands = [p.hand for p in gs.players]
-        for p in gs.players:
-            print(f'Player {p} scored - {p.hand.score(hands)} points')
-
-        max_score = 0
-        winner = ''
-        for p in gs.players:
-            cur_score = p.hand.score(hands)
-            if cur_score > max_score:
-                winner = p
-                max_score = cur_score
-
-        print(f'Winner - {winner}')
+            current_player = self.game_state.players[self.game_state.current_player]
+            row = current_player.choose_row(self.game_state.rows, self.game_state.players)
+            self.game_state.take_row(self.game_state.current_player, row)
+            self.game_state.current_player = (self.game_state.current_player + 1) % len(self.game_state.players)
 
     @staticmethod
     def print_rows(rows):
-        for j in range(3):
-            print(f'{j + 1}: {rows[j]}')
+        for _ in range(3):
+            print(f'{_ + 1}- колода: {rows[_]}')
 
-    @classmethod
-    def is_exist_last_game(cls):
+    @staticmethod
+    def print_player_results(game_state):
+        for player in game_state.players:
+            print(f'{player} - {player.hand.score()}')
+            if player.hand.score() > res:
+                res = player.hand.score()
+        print(f'{player} (набрал {res} очков)')
+        print(f'that a grande vittoria')
+
+    @staticmethod
+    def print_rows(row: list[Row]):
+        for j in range(3):
+            print(f'{j + 1}: {row[j]}')
+
+    @staticmethod
+    def is_exist_last_game():
         try:
-            GameState.load(cls.SAVE_FILE)
+            GameState.load(GameInteractions.SAVE_FILE)
         except Exception:
             return False
         return True
 
-    @classmethod
-    def start_new_game(cls):
+    @staticmethod
+    def start_new_game():
         count_players = 0
-        while not 1 < count_players <= cls.MAX_PLAYERS:
-            count_players = int(input(f'Enter the number of players (2 =< n =< {cls.MAX_PLAYERS}): '))
+        while not 1 < count_players <= GameInteractions.MAX_PLAYERS:
+            count_players = int(input(f'Введите количество игроков (2 =< n =< {GameInteractions.MAX_PLAYERS}): '))
 
         count_humans = -1
-        while not 0 <= count_humans <= count_players:
-            count_humans = int(input(f'Enter the number of players (0 =< n =< {count_players}): '))
+        while not 0 <= count_humans <= count_humans:
+            count_humans = int(input(f'Введите число людей (0 =< n =< {count_players}): '))
 
         players = []
         for i in range(count_humans):
@@ -305,7 +287,7 @@ class GameInteractions:
             players.append(Player(name, None, True))
 
         for i in range(count_players - count_humans):
-            name = 'AI' + str(i)
+            name = f'AI{i}'
             players.append(Player(name, None, False))
 
         current_player = 0
@@ -318,7 +300,6 @@ class GameInteractions:
         rows[1].add_card(deck.draw_card())
 
         return GameState(players, rows, current_player, deck)
-
 
 if __name__ == 'main':
     GameInteractions.run()
