620cecb47f5a0912cce89d3b32103f0ad9c6524f
AlexChilin <super.chilin@mail.ru>
2024-07-04 15:13:26 +0300

    Update Hand.py

--- Hand.py
+++ Hand.py
@@ -1,44 +1,47 @@
 from Card import Card
-import json
 
 
 class Hand:
-    def __init__(self):
-        self.cards = []
+    def __init__(self, cards=None):
+        if cards is None:
+            cards = []
+        self.cards = cards
 
     def __str__(self):
-        return ', '.join(str(card) for card in self.cards)
+        return ', '.join(map(str, self.cards))
 
     def __repr__(self):
-        return ' '.join(str(card) for card in self.cards)
+        return ' '.join(map(str, self.cards))
 
-    def add_cards(self, cards):
-        for _ in cards:
-            self.cards.append(cards)
+    def add_cards(self, cards: list[Card]):
+        for c in cards:
+            self.cards.append(c)
+
+    def score(self, hands: list['Hand'] = None):
+        card_counts = self.get_dictionary()
+        s = 0
+        for k, v in card_counts.items():
+            leader = False
+            if k in Card.SPETSCARDS:
+                if hands is not None:
+                    counts = [list(map(str, hand.cards)).count(k) for hand in hands]
+                    leader = (max(counts) == v)
+            s += Card.score(k, v, leader)
+        return s
 
     def get_dictionary(self):
         card_counts = dict()
         for c in self.cards:
-            card_counts[c] += 1
+            card = c.kind
+            if card not in card_counts:
+                card_counts[card] = 0
+            card_counts[card] += 1
         return card_counts
 
-    def score(self):
-        card_counts = self.get_dictionary()
-        points = 0
-        for c, m in card_counts.items():
-            points += Card.score(c, m)
-        return points
-
     def save(self):
-        return json.dumps({'cards': [card.save() for card in self.cards]})
+        return [c.save() for c in self.cards]
 
     @classmethod
     def load(cls, data):
-        data = json.loads(data)
-        cards = [Card.load(card_data) for card_data in data['cards']]
-        hand = cls()
-        hand.add_cards(cards)
-        return hand
-
-
-    
+        cards = [Card.load(card) for card in data]
+        return Hand(cards)
